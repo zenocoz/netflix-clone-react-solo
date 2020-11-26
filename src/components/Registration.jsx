ddd144bc5d444b54ee031b78afe12a33c2a8241a
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2020-11-26 16:44:01 +0100

    completed form cycle

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,101 +1,134 @@
 import React, {Component} from "react"
-import {Form} from "react-bootstrap"
+import {Form, Container, Row, Col} from "react-bootstrap"
 
 export default class Registration extends Component {
-  state = {}
+  state = {
+    registration: {
+      name: "",
+      surname: "",
+      email: "",
+      password: "",
+      yearOfBirth: "",
+      address: "",
+      city: "",
+      postalCode: "",
+    },
+  }
+
+  handleForm = (e) => {
+    let updatedRegistration = {...this.state.registration}
+    let currentId = e.currentTarget.id
+    updatedRegistration[currentId] = e.currentTarget.value
+    this.setState({registration: updatedRegistration})
+  }
 
   render() {
     return (
-      <Form>
-        <Form.Group>
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Name"
-            value=""
-            id="name"
-            required
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Surname</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="surname"
-            value=""
-            id="surname"
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Email </Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="email"
-            value=""
-            id="email"
-            required
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="passowrd"
-            value=""
-            id="password"
-            required
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Year Of Birth</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="year of birth"
-            value=""
-            id="birth"
-            required
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="address"
-            value=""
-            id="address"
-            required
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>City</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="city"
-            value=""
-            id="city"
-            required
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Postal Code</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="postal code"
-            value=""
-            id="cap"
-            required
-          />
-        </Form.Group>
-        {/* <Form.Group>
-          <Form.Label>Credit Card</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="postal code"
-            value=""
-            id="cap"
-          />
-        </Form.Group> */}
-      </Form>
+      <Container>
+        <Row>
+          <Col className="col-6 offset-3">
+            <Form>
+              <Form.Group>
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Name"
+                  value={this.state.registration.name}
+                  id="name"
+                  onChange={this.handleForm}
+                  required
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Surname</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="surname"
+                  value={this.state.registration.surname}
+                  id="surname"
+                  onChange={this.handleForm}
+                  required
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Email </Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="email"
+                  value={this.state.registration.email}
+                  id="email"
+                  onChange={this.handleForm}
+                  required
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="passowrd"
+                  value={this.state.registration.passowrd}
+                  id="password"
+                  onChange={this.handleForm}
+                  required
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Year Of Birth</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="year of birth"
+                  value={this.state.registration.yearOfBirth}
+                  id="yearOfBirth"
+                  onChange={this.handleForm}
+                  required
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Address</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="address"
+                  value={this.state.registration.address}
+                  id="address"
+                  onChange={this.handleForm}
+                  required
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="city"
+                  value={this.state.registration.city}
+                  id="city"
+                  onChange={this.handleForm}
+                  required
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Postal Code</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="postal code"
+                  value={this.state.registration.cap}
+                  id="cap"
+                  onChange={this.handleForm}
+                  required
+                />
+              </Form.Group>
+              {/* <Form.Group>
+                      <Form.Label>Credit Card</Form.Label>
+                      <Form.Control
+                        type="number"
+                        placeholder="postal code"
+                        value=""
+                        id="cap"
+                      />
+                    </Form.Group> */}
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
